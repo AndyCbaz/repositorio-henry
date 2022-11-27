310c82ebb986f5fc1d147bf9935e4ab0f2d11874
Andrés Bonilla <andy.cbr.tr@gmail.com>
2022-11-26 21:53:03 -0500

    Actualizacion

--- archivo-ejemplo.js
+++ archivo-ejemplo.js
@@ -1,12 +1,19 @@
-function matriz(n,m){
-    var j=0;
-    var i=0;
-    while(i<n){
-        while(j<m){
-            console.log(i + ' ' + j);
-            j++;
-        }
-        j=0;
-        i++;
+// objetos
+
+function alumnos(nombre, apellido, curso){
+    this.nombre = nombre  || 'Pepe';
+    this. apellido = apellido;
+    this. curso = curso;
+    this.saludo = function(){
+        return('Hola mi nombre es '+ this.nombre + ' ' + this.apellido + ' y estoy en ' + this.grado + 'de programación');
     }
 }
+
+var Andrés = new alumnos('Andrés','Bonilla','Primero');
+console.log(Andrés.saludo());
+
+/* keyword new 
+ * crea un objeto vacio var obj = {}
+ * invoca la funcion constructora con los parametros
+ * y de ahi "bindea" (asigna un valor a this y lo designa como propiedad del objeto vacio)
+ * returna el objeto creado*/
